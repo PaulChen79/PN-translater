a2430e035aafe6c399e2d8bfe496f43708704bc0
Pin Hsu Chen <paul791111@gmail.com>
2022-04-21 12:33:24 +0800

    Bug fix for login invalid message

--- config/passport.js
+++ config/passport.js
@@ -18,11 +18,11 @@ module.exports = (app) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: "這個 Email 尚未註冊過" })
+          return done(null, false, req.flash("warning_msg", "這個 Email 尚未註冊過"))
         }
         return bycrypt.compare(password, user.password).then(isMatch => {
           if (!isMatch) {
-            return done(null, false, { message: "密碼輸入錯誤" })
+            return done(null, false, req.flash("warning_msg", "密碼輸入錯誤"))
           }
           return done(null, user)
         })
